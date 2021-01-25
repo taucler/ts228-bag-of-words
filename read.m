d251c13374acd962ee009185853821fbc2b1a110
BREJON Louis <Louis.Brejon@bordeaux-inp.fr>
2021-01-25 15:19:16 +0100

    Ajout des fonctions key_points + k_means. Affichage des key-points dans le fichier read.m

--- read.m
+++ read.m
@@ -2,25 +2,36 @@ clear all;
 clc;
 close all;
 
+%% Parameters
+
+path_avions = 'entrainement/avions';
+
 final_img = [];
+nb_img = 3;
 len = 8;
-for i=1:15
-    s = sprintf('C:/Users/marye/Desktop/sac de mots/images/entrainement/avions/%d.jfif',i);
+
+for i = 1:nb_img
+    s = sprintf('entrainement/avions/%d.jfif',i);
     A = imread(s);
-    figure(i);
-    imshow(A);
-    img = imresize(A, [len len]);
+%     figure(i);
+%     imshow(A);
+    key_points_res = key_points(A);
+
+%     img = imresize(A, [len len]);
+%     
+%     my_img = [];
+%     for j = 1:len
+%         my_img = [my_img, img(j, :)];
+%     end
+%     
+%     my_img = double(my_img);
+%     my_img = my_img ./ sum(my_img);
+%     my_img = my_img - mean(my_img);
+%     
+%     final_img = [final_img; my_img];
+%     figure(i+nb_img);
+%     imagesc(final_img);
     
-    my_img = [];
-    for j = 1:len
-        my_img = [my_img, img(j, :)];
-    end
-    
-    my_img = double(my_img);
-    my_img = my_img ./ sum(my_img);
-    my_img = my_img - mean(my_img);
-    
-    final_img = [final_img; my_img];
-    figure(i+15);
-    imagesc(final_img);
 end
+
+
